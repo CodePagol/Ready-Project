c7fa3da7b1b6977d0238550e1ce1b10e34acb58c
rohan9222 <rohan9222@gmail.com>
2025-04-17 15:55:23 +0600

    2nd

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Spatie\Health\Http\Controllers\HealthCheckResultsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('health', HealthCheckResultsController::class);
 
 Route::middleware([
     'auth:sanctum',
