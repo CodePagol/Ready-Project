e350fbbb7e13655f5a386c2144d322300461428d
rohan9222 <rohan9222@gmail.com>
2025-04-23 01:52:16 +0600

    7th

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,53 @@
 <x-guest-layout>
-    <div class="card mt-5 mx-auto shadow-sm p-4" style="max-width: 400px;">
-        <div class="text-center mb-4">
-            <x-authentication-card-logo class="w-50" />
-        </div>
+    <div class="container">
+		<div class="box">
+			<input type="checkbox" id="toggle" class="box__toggle" hidden>
+			<img src="{{ asset('images/network.png') }}" alt="Picture" class="box__image">
+			<div class="form form--register"></div>
 
-        <x-validation-errors class="mb-4" />
+            <form class="form form--login" method="POST" action="{{ route('login') }}">
+                @csrf
+				<h2 class="form__title">Sign in</h2>
+                <x-validation-errors class="mb-4" />
 
-        @session('status')
-            <div class="alert alert-success" role="alert">
-                {{ $value }}
-            </div>
-        @endsession
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <div class="mb-3">
-                <label for="email" class="form-label">{{ __('Email') }}</label>
-                <input id="email" type="email" name="email" value="{{ old('email') }}" class="form-control" required autofocus autocomplete="username">
-            </div>
-
-            <div class="mb-3">
-                <label for="password" class="form-label">{{ __('Password') }}</label>
-                <input id="password" type="password" name="password" class="form-control" required autocomplete="current-password">
-            </div>
-
-            <div class="form-check mb-3">
-                <input type="checkbox" class="form-check-input" id="remember_me" name="remember">
-                <label class="form-check-label" for="remember_me">{{ __('Remember me') }}</label>
-            </div>
-
-            <div class="d-flex justify-content-between align-items-center">
+                <div class="input-group">
+                    <input id="email" type="email" name="email" value="{{ old('email') }}" class="form-control" required  autocomplete="username" placeholder="E-mail / User Name">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-bounding-box" viewBox="0 0 16 16">
+                            <path d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5M.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5"/>
+                            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                        </svg>
+                    </span>
+                </div>
+                <div class="input-group">
+                    <input t id="password" type="password" name="password" placeholder="Password" required autocomplete="current-password">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key" viewBox="0 0 16 16">
+                            <path d="M0 8a4 4 0 0 1 7.465-2H14a.5.5 0 0 1 .354.146l1.5 1.5a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0L13 9.207l-.646.647a.5.5 0 0 1-.708 0L11 9.207l-.646.647a.5.5 0 0 1-.708 0L9 9.207l-.646.647A.5.5 0 0 1 8 10h-.535A4 4 0 0 1 0 8m4-3a3 3 0 1 0 2.712 4.285A.5.5 0 0 1 7.163 9h.63l.853-.854a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.793-.793-1-1h-6.63a.5.5 0 0 1-.451-.285A3 3 0 0 0 4 5"/>
+                            <path d="M4 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+                        </svg>
+                    </span>
+                </div>
+                    
+                <label class="checkbox-group">
+                    <input type="checkbox" id="remember_me" name="remember">
+                    {{ __('Remember me') }}
+                </label>
                 @if (Route::has('password.request'))
-                    <a wire:navigate.hover href="{{ route('password.request') }}" class="text-decoration-none">{{ __('Forgot your password?') }}</a>
+                    <p class="form__text">
+                        <a wire:navigate.hover class="form__link" href="{{ route('password.request') }}">{{ __('Forgot your password?') }}</a>
+                    </p>
                 @endif
-                <button type="submit" class="btn btn-primary ms-3 p-2">{{ __('Log in') }}</button>
-            </div>
-            <div class="d-flex justify-content-between align-items-center">
+                <button type="submit" class="form__button">Sign In</button>
                 @if (Route::has('register'))
-                    <a wire:navigate.hover href="{{ route('register') }}" class="text-decoration-none text-ellipsis text-inherit text-info" >
-                        Create an account ?
-                    </a>
+                    <p class="form__text">
+                        Don't have an account?
+                        <label for="toggle" class="form__text">
+                            <a wire:navigate.hover class="form__link" href="{{ route('register') }}" >Sign up!</a>
+                        </label>
+                    </p>
                 @endif
-            </div>
-        </form>
-    </div>
+			</form>
+		</div>
+	</div>
 </x-guest-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <x-authentication-card>
+    {{-- <x-authentication-card>
         <x-slot name="logo">
             <x-authentication-card-logo />
         </x-slot>
@@ -16,12 +16,12 @@
 
             <div class="mt-4">
                 <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+                <x-input class="block mt-1 w-full" id="email" type="email" name="email" :value="old('email')" required autocomplete="username" />
             </div>
 
             <div class="mt-4">
                 <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                <x-input class="block mt-1 w-full" id="password" type="password" name="password" autocomplete="new-password" />
             </div>
 
             <div class="mt-4">
@@ -56,5 +56,96 @@
                 </x-button>
             </div>
         </form>
-    </x-authentication-card>
+    </x-authentication-card> --}}
+    <div class="container">
+		<div class="box">
+			<input type="checkbox" id="toggle" class="box__toggle" checked hidden>
+			<img src="{{ asset('images/network.png') }}" alt="Picture" class="box__image">
+            <form class="form form--register" method="POST" action="{{ route('register') }}">
+                @csrf
+                <h2>Sign Up</h2>
+                <x-validation-errors class="mb-4" />
+                <div class="input-group">
+                    <input id="name" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" placeholder="Name">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-bounding-box" viewBox="0 0 16 16">
+                            <path d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5M.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5"/>
+                            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                        </svg>
+                    </span>
+                </div>
+                <div class="input-group">
+                    <input id="email" type="email" name="email" :value="old('email')" required autocomplete="username" placeholder="E-mail">
+                    <span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+                        <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
+                      </svg></span>
+                </div>
+                <div class="input-group">
+                    <input id="password" type="password" name="password" autocomplete="new-password" placeholder="Password" required>
+                    <span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key" viewBox="0 0 16 16">
+                        <path d="M0 8a4 4 0 0 1 7.465-2H14a.5.5 0 0 1 .354.146l1.5 1.5a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0L13 9.207l-.646.647a.5.5 0 0 1-.708 0L11 9.207l-.646.647a.5.5 0 0 1-.708 0L9 9.207l-.646.647A.5.5 0 0 1 8 10h-.535A4 4 0 0 1 0 8m4-3a3 3 0 1 0 2.712 4.285A.5.5 0 0 1 7.163 9h.63l.853-.854a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.793-.793-1-1h-6.63a.5.5 0 0 1-.451-.285A3 3 0 0 0 4 5"/>
+                        <path d="M4 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+                    </svg></span>
+                </div>
+                <div class="input-group">
+                    <input id="password_confirmation" type="password" name="password_confirmation" autocomplete="new-password" placeholder="Password" required>
+                    <span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key" viewBox="0 0 16 16">
+                        <path d="M0 8a4 4 0 0 1 7.465-2H14a.5.5 0 0 1 .354.146l1.5 1.5a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0L13 9.207l-.646.647a.5.5 0 0 1-.708 0L11 9.207l-.646.647a.5.5 0 0 1-.708 0L9 9.207l-.646.647A.5.5 0 0 1 8 10h-.535A4 4 0 0 1 0 8m4-3a3 3 0 1 0 2.712 4.285A.5.5 0 0 1 7.163 9h.63l.853-.854a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.793-.793-1-1h-6.63a.5.5 0 0 1-.451-.285A3 3 0 0 0 4 5"/>
+                        <path d="M4 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+                    </svg></span>
+                </div>
+                <label class="checkbox-group">
+                    <input type="checkbox" required>
+                    I read and agree to <a href="#">Terms & Conditions</a>
+                </label>
+                <button type="submit" class="btn">Create Account</button>
+                <p>Already have an account? <a wire:navigate.hover class="form__link" href="{{ route('login') }}" >Sign in!</a></p>
+            </form>
+
+
+            {{-- login form not--}}
+            <form class="form form--login" method="POST" action="{{ route('login') }}">
+                @csrf
+				<h2 class="form__title">Sign in</h2>
+                <x-validation-errors class="mb-4" />
+                <div class="input-group">
+                    <input id="email" type="email" name="email" value="{{ old('email') }}" class="form-control" required  autocomplete="username" placeholder="E-mail / User Name">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-bounding-box" viewBox="0 0 16 16">
+                            <path d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5M.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5"/>
+                            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                        </svg>
+                    </span>
+                </div>
+                <div class="input-group">
+                    <input t id="password" type="password" name="password" placeholder="Password" required autocomplete="current-password">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key" viewBox="0 0 16 16">
+                            <path d="M0 8a4 4 0 0 1 7.465-2H14a.5.5 0 0 1 .354.146l1.5 1.5a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0L13 9.207l-.646.647a.5.5 0 0 1-.708 0L11 9.207l-.646.647a.5.5 0 0 1-.708 0L9 9.207l-.646.647A.5.5 0 0 1 8 10h-.535A4 4 0 0 1 0 8m4-3a3 3 0 1 0 2.712 4.285A.5.5 0 0 1 7.163 9h.63l.853-.854a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.793-.793-1-1h-6.63a.5.5 0 0 1-.451-.285A3 3 0 0 0 4 5"/>
+                            <path d="M4 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+                        </svg>
+                    </span>
+                </div>
+                    
+                <label class="checkbox-group">
+                    <input type="checkbox" id="remember_me" name="remember">
+                    {{ __('Remember me') }}
+                </label>
+                @if (Route::has('password.request'))
+                    <p class="form__text">
+                        <a wire:navigate.hover class="form__link" href="{{ route('password.request') }}">{{ __('Forgot your password?') }}</a>
+                    </p>
+                @endif
+                <button type="submit" class="form__button">Sign In</button>
+                @if (Route::has('register'))
+                    <p class="form__text">
+                        Don't have an account?
+                        <label for="toggle" class="form__text">
+                            <a wire:navigate.hover class="form__link" href="{{ route('register') }}" >Sign up!</a>
+                        </label>
+                    </p>
+                @endif
+			</form>
+		</div>
+	</div>
 </x-guest-layout>
